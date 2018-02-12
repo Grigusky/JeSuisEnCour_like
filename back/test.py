963486d19ec03b28784a2cfc526b62d166d8b1b7
grigusky <gregoire.meunier@live.fr>
2018-02-12 15:20:51 +0100

    ajout de test 1

--- back/test.py
+++ back/test.py
@@ -1,4 +1,13 @@
+import requests
+
+import pytest
 
 
-def test():
-    return "ok"
+@pytest.fixture
+def exemple_html():
+    r = requests.get('https://www.google.fr/')
+    return r.status_code
+
+
+def test_status_code(exemple_html):
+    assert exemple_html == 200
