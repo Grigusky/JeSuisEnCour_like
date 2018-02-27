6195f3f49454e8224fb7b99ee8d3a7c484139025
grigusky <gregoire.meunier@live.fr>
2018-02-27 14:28:46 +0100

    ajout des settings et urls

--- back/back/settings.py
+++ back/back/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'administration.apps.AdministrationConfig',
-    'rest_framework'
+    'rest_framework_swagger'
 ]
 
 MIDDLEWARE = [
